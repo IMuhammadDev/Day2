155804325841153e737d148ca4cfc2644f8e9c87
IkromSWI <111283447+IkromSWI@users.noreply.github.com>
2023-05-05 16:40:23 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+# This lesson about data types and how to know which  data type .
+
+# 1-String famous by str in python. This type is consist of text or characters
+name = "Abdulloh"
+auto_mark_is = "BMW"
+
+# 2-Integer famous by int . This type is consist of numbers without points
+number = 0
+age = 40
+
+# 3-Float famous by float . This type is consist of numbers with points
+ball = 5.5
+sum_of_salary = 120.1
+
+# 4-Boolean famous by bool . This type is logical values that can only be True or False.
+con = True
+result = False
+
+# How to know data type
+type_of_same_thing = 0
+print(type(sum_of_salary))
+
+
+
+
+
